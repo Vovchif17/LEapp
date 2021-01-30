1b3714e2143673b5318c1548acb505d65417750f
vovchif17 <vovchif17@gmail.com>
2021-01-30 12:23:10 -0500

    Make a basic User model (include secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -18,12 +18,19 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test"password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6 
+  test "email addreses should be unique" do
+  duplicate_user = @user.dup
+  #duplicate_user.email = @user.email.upcase
+  @user.save
+  assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
 
-  test"password should have a minimum length" do 
+  test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
